0d7105f1d6975d34200d205aa40b6f637b634de2
juancarlangas <juancarlangas@gmail.com>
2023-03-02 15:20:35 -0600

    Primer feedback formal. Vamos a darle formato.
    Necesitamos meterle tabuladores en vez de espacios. Ya le avancé
    bastante pero te toca, jijiji.

--- src/main.cpp
+++ src/main.cpp
@@ -1,156 +1,95 @@
-/*HERENCIA MULTIPLE PARTE 2
+/*HERENCIA MULTIPLE PARTE 2 */
 
-Clase padre:
-Persona Carrera Area
+/* Relajar tu mente significa poder "elevarte". En realidad vas más profundo, pero al mismo tiempo
+ * te elevas. Debo decirte que me encantó tu proyecto. Tienes mucho talento, y salvo tu mejor
+ * opinión, tienes madera de programador. Espero poder estar a la altura de lo que necesitas para
+ * brillar.
+ *
+ * Es un proyecto muy interesante. Cuando te elevas, siempre puedes ver un cuadro más grande, un
+ * big picture, y sabrías que un híbrido entre una "persona" y una "clase escolar" sería digna
+ * de la película 'La Mosca'. Más que un objeto "compuesto" me parece la Abominación de Hulk jeje,
+ * pero te salió al puro pedo. Mi experiencia con herencia múltiple me hace pensar que lo adecuado
+ * es combinar objetos con objetos y no objetos con servicios o elementos intangibles (clases,
+ * empleos, etc). Aquí se hubiera resuelto de manera más sencilla con herencia simple, pero lograste
+ * el fenómeno :P y te felicito. De verdad me emocionan mucho tus posibilidades decidas lo que
+ * decidas hacer
+ *
+ * En otro aspecto, quisiera ver cómo estás configurando VSCode. Hay una opción que te convierte
+ * tabuladores en espacios. Esto resulta adecuado para visualizar tu código en editores no muy
+ * pro (GitHub, Discord, etc) pero resulta contraproducente entre editores profesionales (Vim,
+ * VSCode, etc), y casi fatal a la hora de producir un makefile (recuerda que la sintaxis está
+ * basada en el caracter TAB: Nadie quiere que desaparezcan por "espacios". Te encargo mucho
+ * homogeneizar la identación con Tabuladores reales para poder seguir trabajándolo. Por ahorita
+ * con esto comenzamos para no abrumarnos */
 
-Clase hija:
-    Estudiantes: Personas, Carrera
-    Empleado: Personas, Areas*/
-
+// Vamos a meter espacio entre #include y la librería
 #include<iostream>
 #include<string>
 
-using std::cin; // declarativa -using- inutilizada.
+/* Declarativa -using- inutilizada. Hay que eliminarla o usarla. Si instalas LLVM con Chocolatey
+ * y compilas con clang++ verás que te brinca al compilar. */
+// using std::cin;
 using std::cout;
 using std::string;
 
-class Personas
-{
-    public: 
-
-        void setNombre(string Nombre)
-        {
-            NombrePersona = Nombre;
-        }
-
-        void setApellido(string Apellido)
-        {
-            ApellidoPersona = Apellido;
-        }
-
-        void setGenero(string Genero)
-        {
-            GeneroPersona = Genero;
-        }
-
-        void setPeso(float Peso)
-        {
-            PesoPersona = Peso;
-        }
-
-        void setEstatura(float Estatura)
-        {
-            EstaturaPesona = Estatura;
-        }
-
-        void setEdad(int Edad)
-        {
-            EdadPersona=Edad;
-        }
-
-        string getNombrePersona()const
-        {
-            return NombrePersona;
-        }
-
-        string getApellidoPersona()const
-        {
-            return ApellidoPersona;
-        }
-
-        string getGeneroPersona()const
-        {
-            return GeneroPersona;
-        }
-
-        float getPesoPersona()const
-        {
-            return PesoPersona;
-        }
-
-        float getEstaturaPersona()const
-        {
-            return EstaturaPesona;
-        }
-
-        int getEdadPersona()const
-        {
-            return EdadPersona;
-        }
-
-
-        void PersonaInfo();
-
-    private:    //DATOS GENERALES DE PERSONA
-        string NombrePersona, ApellidoPersona, GeneroPersona;
-        float PesoPersona, EstaturaPesona;
-        int EdadPersona;
+/* Te sugiero el nombre -Persona-, porqe "Personas" se referiría a VARIAS y no representa
+ * fielmente al "objeto".
+ *
+ * Te recomiendo también nombres con guiones_bajos en lugar de tipo camelCase. Así está construido
+ * el lenguaje. Además te recomiendo identaciones sencillas. Lo modificaré para trabajar a gusto */
+class Personas {
+	public:
+		void setNombre( string Nombre ) { NombrePersona = Nombre; }
+		void setApellido( string Apellido ) { ApellidoPersona = Apellido; }
+		void setGenero( string Genero ) { GeneroPersona = Genero; }
+		void setPeso( float Peso ) { PesoPersona = Peso; }
+		void setEstatura( float Estatura ) { EstaturaPesona = Estatura; }
+		void setEdad( int Edad ) { EdadPersona = Edad; }
+		string getNombrePersona() const { return NombrePersona; }
+		string getApellidoPersona() const { return ApellidoPersona; }
+		string getGeneroPersona() const { return GeneroPersona; }
+		float getPesoPersona() const { return PesoPersona; }
+		float getEstaturaPersona() const { return EstaturaPesona; }
+		int getEdadPersona() const { return EdadPersona; }
+		void PersonaInfo(); // Esto se debería llamar imprimir_info() o algo así
+	private:
+		/* Porque nombrePersona si la clase YA ES persona?
+		 *
+		 * En los atributos se recomienda que las variablew tengan su propio renglón */
+		string NombrePersona, ApellidoPersona, GeneroPersona;
+		float PesoPersona, EstaturaPesona;
+		int EdadPersona;
 };
 
-//CARRERAS PARA EL ESTUDIANTE
-class Carreras
-{
-    
-    public: 
-        void setCarreraEstudiante(string Carrera)
-        {
-            CarreraEstudiante = Carrera;
-        }
-
-        string getCarreraEstudiante()const
-        {
-            return CarreraEstudiante;
-        }
-
-    private:
-        string CarreraEstudiante;
+class Carreras {
+	//CARRERAS PARA EL ESTUDIANTE
+	/* El título anterior no me gusta. Se supone que apenas la vas a heredar para que los métodos
+	 * set y get formen parte de la clase hija Estudiantes. El confinarlo desde ahora "para
+	 * estudiantes" no lo hace muy bien visto */
+	public:
+		void setCarreraEstudiante(string Carrera) { CarreraEstudiante = Carrera; }
+		string getCarreraEstudiante()const { return CarreraEstudiante; }
+	private:
+		string CarreraEstudiante;
 };
 
-//AREAS DONDE TRABAJA EL EMPLEADO
-class Areas
-{
-    public: 
-        void setAreaEmpleado(string Area)
-        {
-            AreaEmpleado = Area;
-        }
+class Areas {
+	//AREAS DONDE TRABAJA EL EMPLEADO
+	public:
+		void setAreaEmpleado(string Area) { AreaEmpleado = Area; }
+		string getAreaEmpleado() const { return AreaEmpleado; }
+	private:
+		string AreaEmpleado;
+	};
 
-        string getAreaEmpleado()const
-        {
-            return AreaEmpleado;
-        }
-
-
-    private: 
-        string AreaEmpleado;
-};
-
-//Clase Hija Estudiantes
-class Estudiantes: public Personas, public Carreras
-{
-    public:
-        void setTurno(string Turno)
-        {
-            TurnoEstudiante = Turno;
-        }
-
-        void setSemestre(int Semestre)
-        {
-            SemestreEstudiante = Semestre;
-        }   
-
-        string getTurnoEstudiante()const
-        {
-            return TurnoEstudiante;
-        }
-
-        int getSemestreEstudiante()const
-        {
-            return SemestreEstudiante;
-        }
-
-        void EstudianteInfo();
-
+class Estudiantes: public Personas, public Carreras {
+	// Clase Hija Estudiantes
+	public:
+		void setTurno(string Turno) { TurnoEstudiante = Turno; }
+        void setSemestre(int Semestre) { SemestreEstudiante = Semestre; }
+        string getTurnoEstudiante() const { return TurnoEstudiante; }
+        int getSemestreEstudiante() const { return SemestreEstudiante; }
+        void EstudianteInfo(); // Igual esto se debería llamar también imprimir_info()
     private:
         string TurnoEstudiante;
         int SemestreEstudiante;
@@ -158,30 +97,13 @@ class Estudiantes: public Personas, public Carreras
 
 class Empleados: public Personas, public Areas
 {
-    public: 
-        void setSueldoEmpleado(float Sueldo)
-        {
-            SueldoEmpleado = Sueldo;
-        }
-        
-        void setFechaIngreso(string Ingreso)
-        {
-            FechaIngresoEmpleado = Ingreso;
-        }
-
-        float getSueldoEmpledo()const
-        {
-            return SueldoEmpleado;
-        }
-
-        string getFechaEmpleado()const
-        {
-            return FechaIngresoEmpleado;
-        }
-
+	public:
+		void setSueldoEmpleado(float Sueldo) { SueldoEmpleado = Sueldo; }
+		void setFechaIngreso(string Ingreso) { FechaIngresoEmpleado = Ingreso; }
+        float getSueldoEmpledo() const { return SueldoEmpleado; }
+        string getFechaEmpleado() const { return FechaIngresoEmpleado; }
         void EmpleadoInfo();
-
-    private: 
+    private:
         float SueldoEmpleado;
         string FechaIngresoEmpleado;
 };
